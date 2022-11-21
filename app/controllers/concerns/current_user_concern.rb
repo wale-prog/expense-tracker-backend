d2870aff7bb255559bddc916081cb6972ab1894a
Olawale <olapetanwale@outlook.com>
2022-11-21 11:44:24 +0100

    Move CurrentUser logic to sessionsController because of a bug

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -2,9 +2,10 @@ module CurrentUserConcern
   extend ActiveSupport::Concern
 
   include do
-    before_action :set_current_user
+    before_action :set_current_user, only: [:create, :logged_in]
   end
 
+  private
   def set_current_user
     if session[:user_id]
       @current_user = User.find(session[:user_id])
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,8 @@
 class SessionsController < ApplicationController
-include CurrentUserConcern
+  before_action :set_current_user 
+
+  # include CurrentUserConcern
+
   def create
     user = User
       .find_by(email: params["user"]["email"])
@@ -17,7 +20,7 @@ include CurrentUserConcern
   end
 
   def logged_in
-    if current_user
+    if @current_user
       render json: {
         logged_in: true,
         user: @current_user
@@ -32,5 +35,12 @@ include CurrentUserConcern
   def logout
     reset session
     render json: { status:200, logged_out: true }
+  end
+
+  private
+  def set_current_user
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    end
   end
 end
